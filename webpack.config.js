6a8746be0b7b5d756d20c840477453c854d9fb93
heresson <heresson@gmail.com>
2021-05-17 10:36:58 -0300

    incluindo assets css

--- webpack.config.js
+++ webpack.config.js
@@ -37,6 +37,12 @@ module.exports = validate({
       exclude: /node_modules/,
       include: /src/,
       loader: 'babel'
+    },
+    {
+      test: /\.css$/,
+      exclude: /node_modules/,
+      include: /src/,
+      loaders: ['style','raw']
     }]
   }
 })
