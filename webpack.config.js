abe427b38757adc5b04ff8009e4478bc8403bf95
heresson <heresson@gmail.com>
2021-05-17 11:46:12 -0300

    ajuste de assets

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,8 @@
 const path = require('path')
 const webpack = require('webpack')
 const validate = require('webpack-validator')
+const HtmlPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = validate({
   devtool: 'source-map',
@@ -16,12 +18,17 @@ module.exports = validate({
 
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/static/'
+    filename: '[name]-[hash].js',
+    publicPath: ''
   },
 
   plugins: [
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
+    new ExtractTextPlugin('[name]-[hash].css'),
+    new HtmlPlugin({
+      title: 'My App',
+      template: path.join(__dirname, 'src', 'html', 'template.html')
+    })
   ],
 
   module: {
@@ -42,7 +49,7 @@ module.exports = validate({
       test: /\.css$/,
       exclude: /node_modules/,
       include: /src/,
-      loaders: ['style','raw']
+      loader: ExtractTextPlugin.extract('style', 'css')
     }]
   }
 })
